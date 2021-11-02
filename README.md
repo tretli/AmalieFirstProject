e0533cedd84cdedf76f315d513ae2ebfc9cd9b09
tretli <thomas.tretli@gmail.com>
2021-11-02 19:41:46 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-vquu7y-livwam
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-vquu7y-livwam)
---
+++ src/app/products.ts
@@ -0,0 +1,188 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+}
+
+export const products = [
+  // {
+  //   id: 1,
+  //   name: 'Phone XL',
+  //   price: 799,
+  //   description: 'A large phone with one of the best screens'
+  // },
+  // {
+  //   id: 2,
+  //   name: 'Phone Mini',
+  //   price: 699,
+  //   description: 'A great phone with one of the best cameras'
+  // },
+  // {
+  //   id: 3,
+  //   name: 'Phone Standard',
+  //   price: 299,
+  //   description: ''
+  // }
+
+  {
+    id: 1,
+    name: 'Kaffe         ',
+    price: 10,
+    category: 1,
+    description: 'Dette er bakt av Amalie',
+  },
+  {
+    id: 1,
+    name: 'Brus          ',
+    price: 10,
+    category: 1,
+    description: 'Dette er bakt av Amalie',
+  },
+  {
+    id: 1,
+    name: 'Te            ',
+    price: 10,
+    category: 1,
+    description: 'Dette er bakt av Amalie',
+  },
+  {
+    id: 1,
+    name: 'Kakao         ',
+    price: 10,
+    category: 1,
+    description: 'Dette er bakt av Amalie',
+  },
+  {
+    id: 1,
+    name: 'Mat           ',
+    price: 10,
+    category: 1,
+    description: 'Dette er bakt av Amalie',
+  },
+  {
+    id: 1,
+    name: 'Baget         ',
+    price: 10,
+    category: 1,
+    description: 'Dette er bakt av Amalie',
+  },
+  {
+    id: 1,
+    name: 'Mat mufins    ',
+    price: 10,
+    category: 1,
+    description: 'Dette er bakt av Amalie',
+  },
+  {
+    id: 1,
+    name: 'Brød          ',
+    price: 10,
+    category: 1,
+    description: 'Dette er bakt av Amalie',
+  },
+  {
+    id: 1,
+    name: 'Runstyke      ',
+    price: 10,
+    category: 1,
+    description: 'Dette er bakt av Amalie',
+  },
+  {
+    id: 1,
+    name: 'Desert        ',
+    price: 10,
+    category: 1,
+    description: 'Dette er bakt av Amalie',
+  },
+  {
+    id: 1,
+    name: 'Kake          ',
+    price: 10,
+    category: 1,
+    description: 'Dette er bakt av Amalie',
+  },
+  {
+    id: 1,
+    name: 'Krosang       ',
+    price: 10,
+    category: 1,
+    description: 'Dette er bakt av Amalie',
+  },
+  {
+    id: 1,
+    name: 'Boller        ',
+    price: 10,
+    category: 1,
+    description: 'Dette er bakt av Amalie',
+  },
+  {
+    id: 1,
+    name: 'Kake',
+    price: 10,
+    category: 1,
+    description: 'Dette er bakt av Amalie',
+  },
+  {
+    id: 1,
+    name: 'Pandesjokolat ',
+    price: 10,
+    category: 1,
+    description: 'Dette er bakt av Amalie',
+  },
+  {
+    id: 1,
+    name: 'Cookie',
+    price: 10,
+    category: 1,
+    description: 'Dette er bakt av Amalie',
+  },
+  {
+    id: 1,
+    name: 'Mufins',
+    price: 10,
+    category: 1,
+    description: 'Dette er bakt av Amalie',
+  },
+  {
+    id: 1,
+    name: 'Kanelbolle    ',
+    price: 10,
+    category: 1,
+    description: 'Dette er bakt av Amalie',
+  },
+  {
+    id: 1,
+    name: 'Sokoladebolle ',
+    price: 10,
+    category: 1,
+    description: 'Dette er bakt av Amalie',
+  },
+  {
+    id: 1,
+    name: 'Skolebolle',
+    price: 10,
+    category: 1,
+    description: 'Dette er bakt av Amalie',
+  },
+  {
+    id: 1,
+    name: 'Sokoladekake',
+    price: 10,
+    category: 1,
+    description: 'Dette er bakt av Amalie',
+  },
+  {
+    id: 1,
+    name: 'Gulerotkake',
+    price: 10,
+    category: 1,
+    description: 'Dette er bakt av Amalie',
+  },
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
